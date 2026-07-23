88a94fe6652ad9792e1bb6a98262d61b7fd17cd0
Dmitry <maiiamphiphi@gmail.com>
2026-07-23 22:01:44 +0700

    connect js for burger-menu

---
+++ scripts/header.js
@@ -0,0 +1,62 @@
+const menu = document.getElementById('burger-menu');
+const menu1level = document.getElementById('burger-menu-1-level');
+const menu2level = document.getElementById('burger-menu-2-level');
+const openBurgerBtn = document.getElementById('burger-open');
+const overlay = document.getElementById('overlay');
+
+let isMenuOpen = false;
+let currMenuLevel = 1;
+
+function openMenu() {
+
+  menu.classList.add("menu__open");
+  overlay.classList.add("active");
+
+  isMenuOpen = true;
+}
+
+function closeMenu() {
+  menu.classList.remove("menu__open");
+  overlay.classList.remove("active");
+  isMenuOpen = false;
+}
+
+function changeMenuLevel() {
+  if (currMenuLevel === 1) {
+    currMenuLevel = 2;
+    menu1level.classList.add('level-2-active');
+    menu1level.classList.remove('level-1-active');
+
+    menu2level.classList.add('level-2-active');
+    menu2level.classList.remove('level-1-active');
+  } else if (currMenuLevel === 2) {
+    currMenuLevel = 1;
+    menu1level.classList.add('level-1-active');
+    menu1level.classList.remove('level-2-active');
+
+    menu2level.classList.add('level-1-active');
+    menu2level.classList.remove('level-2-active');
+  }
+}
+
+openBurgerBtn.addEventListener("click", openMenu);
+
+document.addEventListener("click", (event) => {
+  const target = event.target;
+  console.log(target)
+
+  if (
+    isMenuOpen &&
+    (
+      target.classList.contains("header__overlay") ||
+      target.classList.contains("menu__close") ||
+      target.closest(".menu__close")
+    )
+  ) {
+    closeMenu();
+  }
+
+  if (target.closest(".toggle-level-menu")) {
+    changeMenuLevel();
+  }
+});
