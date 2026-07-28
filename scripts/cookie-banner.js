7c3b72e03a0d014668dbb50592097d85435388c4
Dmitry <maiiamphiphi@gmail.com>
2026-07-28 18:38:57 +0700

    add cookie banner

---
+++ scripts/cookie-banner.js
@@ -0,0 +1,35 @@
+(() => {
+  const STORAGE_KEY = "cookieBannerClosed";
+  const VISIBLE_CLASS = "cookie-banner--visible";
+  const ANIMATION_DURATION = 500;
+
+  const banner = document.querySelector(".cookie-banner");
+  if (!banner) return;
+
+  const acceptButton = banner.querySelector(".cookie-banner__button");
+  const isClosed = localStorage.getItem(STORAGE_KEY) === "true";
+
+  if (!isClosed) {
+    showBanner();
+  }
+
+  if (acceptButton) {
+    acceptButton.addEventListener("click", closeBanner);
+  }
+
+  function showBanner() {
+    setTimeout(() => {
+      banner.classList.add(VISIBLE_CLASS);
+    }, 1000);
+  }
+
+  function closeBanner() {
+    console.log(1)
+    banner.classList.remove(VISIBLE_CLASS);
+    localStorage.setItem(STORAGE_KEY, "true");
+
+    setTimeout(() => {
+      banner.remove();
+    }, ANIMATION_DURATION);
+  }
+})();
